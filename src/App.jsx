a35a12870a38ab20ef46839e8eb2f656b84c26f7
Naoki Kasahara <kasane1101@gmail.com>
2026-01-07 22:57:34 +0900

    fix: correct BrowserRouter wrapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import CareerCounselorRoleplay from './CareerCounselorRoleplay'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Scenario from "./pages/Scenario";
+import RolePlay from "./pages/RolePlay";
+import Result from "./pages/Result";
+import Transcript from "./pages/Transcript";
+import Header from "./components/Header";
 
-function App() {
-  return <CareerCounselorRoleplay />
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/scenario" element={<Scenario />} />
+        <Route path="/roleplay" element={<RolePlay />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/transcript" element={<Transcript />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
